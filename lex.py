a05a7f4286264abfdea90e0597689502983f0f6e
fonarmeister <minidoc004@gmail.com>
2022-10-02 20:22:15 +0300

    description, examples and lexer

--- lex.py
+++ lex.py
@@ -1,65 +1,69 @@
 import ply.lex as lex
 import sys
 
-# 1 + 2 * x123 + if x then 31 else 42
-
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+  'to': 'RULER'
 }
 
 tokens = [
-  'NUM',
-  'PLUS',
-  'MINUS',
-  'MULT',
-  'DIV',
-  'POW',
-  'ID',
-  'LBR',
-  'RBR'
+  'TERM',
+  'NONTERM',
+  'START',
+  'EPS'
 ] + list(reserved.values())
 
-def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
-  return t
 
-def t_NUM(t):
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
+def t_TERM(t):
+    r't\'[ -~]+\'t'
+    t.value = t.value[2:-2]
+    return t
 
-t_PLUS = r'\+'
-t_MINUS = r'\-'
-t_MULT = r'\*'
-t_DIV = r'\/'
-t_POW = r'\*\*'
-t_LBR = r'\('
-t_RBR = r'\)'
 
-t_ignore = ' \t'
+def t_NONTERM(t):
+    r'n\'[ -~]+\'n'
+    t.value = t.value[2:-2]
+    return t
+
+
+def t_START(t):
+    r'set\_start [ -~]+'
+    t.value = t.value[11:]
+    return t
+
+
+t_EPS = r'eps'
+
 
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_error(t):
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 
 lexer = lex.lex()
 
-def main():
-  lexer = lex.lex()
-  lexer.input(sys.argv[1])
 
-  while True:
-    tok = lexer.token()
-    if not tok:
-      break
-    print(tok)
+def run_tests():
+    
+
+
+def main():
+    run_tests()
+
+    input_count = len(sys.argv)
+    for i in range(1, len):
+        with open(sys.argv[i], "r") as input:
+            with open(sys.argv[i], "w") as output:
+                for line in input.readlines():
+                    lexer.input(line)
+                    cur_token = lexer.token()
+                    while cur_token:
+                        print(cur_token, end = '\n')
+                        cur_token = lexer.token()
 
 if __name__ == "__main__":
     main()
