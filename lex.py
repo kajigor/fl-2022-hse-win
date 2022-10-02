cb8c6f976d7aeed9d4151dd0912b139e0710b9d0
Taifin <tralllgm@gmail.com>
2022-10-02 23:31:42 +0300

    Task2

--- lex.py
+++ lex.py
@@ -1,65 +1,72 @@
 import ply.lex as lex
 import sys
 
-# 1 + 2 * x123 + if x then 31 else 42
-
 reserved = {
-  'if': 'IF',
-  'then': 'THEN',
-  'else': 'ELSE'
+    'start': 'START',
 }
 
 tokens = [
-  'NUM',
-  'PLUS',
-  'MINUS',
-  'MULT',
-  'DIV',
-  'POW',
-  'ID',
-  'LBR',
-  'RBR'
-] + list(reserved.values())
+             'TERM',
+             'NTERM',
+             'EPSILON',
+             'BIND',
+             'OR',
+             'END_OF_RULE'
+         ] + list(reserved.values())
 
-def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
-  return t
+t_ignore = r' \t'
+t_OR = r'\|'
+t_BIND = r'='
+t_EPSILON = r'\#eps\#'
+t_END_OF_RULE = r';'
 
-def t_NUM(t):
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
+error = False
 
-t_PLUS = r'\+'
-t_MINUS = r'\-'
-t_MULT = r'\*'
-t_DIV = r'\/'
-t_POW = r'\*\*'
-t_LBR = r'\('
-t_RBR = r'\)'
 
-t_ignore = ' \t'
+def clean(t):
+    return t.replace("\\\$", "\$").replace("\\\\", "\\")
+
 
 def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 
 def t_error(t):
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
+    global error
+    print("Illegal character {0} at position {1} at line {2}".format(t.value[0], t.lexer.lexpos, t.lexer.lineno))
+    print("Cannot proceed analysis, exiting...")
+    error = True
+    t.lexer.skip(1)
 
-lexer = lex.lex()
+
+def t_TERM(t):
+    r'\$[^$\\]+((\\\$)?[^$\\]*)*\$'
+    t.value = clean(t.value[:])
+    return t
+
+
+def t_NTERM(t):
+    r'\#[^#\\]+((\\\#)?[^#\\]*)*\#'
+    t.value = clean(t.value[:])
+    return t
+
 
 def main():
-  lexer = lex.lex()
-  lexer.input(sys.argv[1])
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
 
-  while True:
-    tok = lexer.token()
-    if not tok:
-      break
-    print(tok)
+        with open(file, 'r') as inf, open(file + ".out", 'w') as outf:
+            lexer = lex.lex()
+            lex_input = "".join(inf.readlines())
+            lexer.input(lex_input)
+
+            while True:
+                tok = lexer.token()
+                if not tok or error:
+                    break
+                print(tok, file=outf)
+
 
 if __name__ == "__main__":
     main()
