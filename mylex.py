e6c00f2395f1aff2adf4a53d4452efb8a454993a
Alina-Muha <AlinaMyxa2002@yandex.ru>
2022-10-05 19:41:38 +0300

    task 2

---
+++ mylex.py
@@ -0,0 +1,71 @@
+import ply.lex as lex
+import sys
+
+tokens = [
+    'TERMINAL',
+    'NON_TERMINAL',
+    'START',
+    'NONE',
+    'ARROW',
+    'SEPARATOR',
+    'ALTERNATIVE'
+]
+
+t_ARROW = r'='
+t_NONE = r'@'
+t_SEPARATOR = r';'
+t_ALTERNATIVE = r'\|'
+t_ignore = " \t"
+
+
+def change(name):
+    return name.replace("\\$", "$").replace("\\&", "&").replace("\\\\", "\\")
+
+
+def t_START(t):
+    r'start=\$([^\\\&\$@] | \\.)*?\$'
+    t.value = change(t.value[7:-1])
+    return t
+
+
+def t_TERMINAL(t):
+    r'\&([^\\\&\$@] | \\.)*?\&'
+    t.value = change(t.value[1:-1])
+    return t
+
+
+def t_NON_TERMINAL(t):
+    r'\$([^\\\&\$@] | \\.)*?\$'
+    t.value = change(t.value[1:-1])
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
+
+def main():
+    lexer = lex.lex()
+    infile = sys.argv[1]
+    outfile = infile.replace(".in", ".out")
+    with open(infile, 'r') as lang, open(outfile, 'w') as result:
+        for line in lang:
+            lexer.input(line)
+            while True:
+                tok = lexer.token()
+                if not tok:
+                    break
+                result.write(str(tok) + "\n")
+
+
+if __name__ == "__main__":
+    main()
