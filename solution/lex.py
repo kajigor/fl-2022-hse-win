26fd4756818de7825cc9e1c997536f3e3232fdfd
mediana105 <st087217@student.spbu.ru>
2022-10-05 23:44:14 +0300

    modified

--- solution/lex.py
+++ solution/lex.py
@@ -27,24 +27,24 @@ t_OR = r'\|'
 
 def t_TERMINAL(t):
     r'\'(([^\\])|(\\[\x00-\x7F]))*?\''
-    t.value = t.value[1:-1].replace("\\'", "'").replace("\\\\", "\\").replace("\\e", "e")
+    t.value = t.value[1:-1].replace("\\'", "'").replace("\\\\", "\\")
     return t
 
 def t_NON_TERMINAL(t):
     r'\$(([^\\])|(\\[\x00-\x7F]))*?\$'
-    t.value = t.value[1:-1].replace("\\$", "$").replace("\\\\", "\\").replace("\\e", "e")
+    t.value = t.value[1:-1].replace("\\$", "$").replace("\\\\", "\\")
     return t
 
 def t_START(t):
     r'start->\$(([^\\])|(\\[\x00-\x7F]))*?\$'
-    t.value = t.value[8:-1].replace("\\$", "$").replace("\\\\", "\\").replace("\\e", "e")
+    t.value = t.value[8:-1].replace("\\$", "$").replace("\\\\", "\\")
     return t
 
 lexer = lex.lex()
 
 def main():
     input_file = sys.argv[1]
-    output_file = input_file.replace(".in", ".out")
+    output_file = input_file + ".out"
     with open(input_file, "r") as file1:
         with open(output_file, "w") as file2:
             for data in file1:
@@ -54,6 +54,7 @@ def main():
                     if not tok:
                         break
                     file2.write(str(tok) + '\n')
+
 
 if __name__ == "__main__":
     main()
