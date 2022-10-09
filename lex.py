d071cb946350c8c27ca329f1c916bc86794c7781
Smart781 <84939218+Smart781@users.noreply.github.com>
2022-10-09 03:24:08 +0300

    Update lex.py

--- lex.py
+++ lex.py
@@ -6,62 +6,63 @@ reserved = {
 }
 
 tokens = [
-             'TERM',
-             'NTERM',
-             'EPSILON',
-             'BIND',
-             'OR',
-             'END'
-         ] + list(reserved.values())
+    'TERMINAL',
+    'NOT_TERMINAL',
+    'START',
+    'EMPTY',
+    'IS',
+    'END',
+    'OR'
+         ] 
 
-t_ignore = r' \t'
+t_IS = r'->'
+t_ignore = r' '
 t_OR = r'\|'
-t_BIND = r'='
-t_EPSILON = r'\*'
+t_EMPTY = r'\*'
 t_END = r';'
 
-error = False
+def t_START(t):
+    r'start->\!([^\\\@\!\*] | \\.)*?\!'
+    t.value = t.value[7:-1].replace("\\@", "@").replace("\\!", "!").replace("\\*", "*").replace("\\\\", "\\")
+    return t
 
-def clean(t):
-    return t.replace("\\\@", "\@").replace("\\\\", "\\")
+
+def t_NOT_TERMINAL(t):
+    r'\!([^\\\@\!\*] | \\.)*?\!'
+    t.value = t.value[7:-1].replace("\\@", "@").replace("\\!", "!").replace("\\*", "*").replace("\\\\", "\\")
+    return t
+
+
+def t_TERMINAL(t):
+    r'\@([^\\\@\!\*] | \\.)*?\@'
+    t.value = t.value[7:-1].replace("\\@", "@").replace("\\!", "!").replace("\\*", "*").replace("\\\\", "\\")
+    return t
+
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-    
+
+
 def t_error(t):
-    global error
-    print("Error")
-    error = True
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
     t.lexer.skip(1)
-    
-def t_TERM(t):
-    r'\@[^@\\]+((\\\@)?[^@\\]*)*\@'
-    t.value = clean(t.value[:])
-    return t
 
 
-def t_NTERM(t):
-    r'\![^!\\]+((\\\!)?[^!\\]*)*\!'
-    t.value = clean(t.value[:])
-    return t
+lexer = lex.lex()
 
 
 def main():
-    if len(sys.argv) > 1:
-        file = sys.argv[1]
-
-        with open(file, 'r') as inf, open(file + ".out", 'w') as outf:
-            lexer = lex.lex()
-            lex_input = "".join(inf.readlines())
-            lexer.input(lex_input)
-
+    lexer = lex.lex()
+    infile = sys.argv[1]
+    with open(infile) as file:
+        for line in file:
+            lexer.input(line)
             while True:
                 tok = lexer.token()
-                if not tok or error:
+                if not tok:
                     break
-                print(tok, file=outf)
-
+                print(tok)
 
 if __name__ == "__main__":
     main()      
