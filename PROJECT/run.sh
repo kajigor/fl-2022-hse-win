ed0ace27c6c04b45ef7ea91e367a1ffcea4d00c8
simkes <simksa03@gmail.com>
2022-10-20 19:05:32 +0300

    made dir

--- PROJECT/run.sh
+++ PROJECT/run.sh
@@ -3,8 +3,8 @@ INPUT=$1
 FILENAME=$(echo "$INPUT" | sed -r -e 's/^(.+)\.txt$/\1/')
 LEXEROUT="${INPUT}.out"
 touch "$LEXEROUT"
-python3 grammar_lexer.py "$INPUT"
+python3 executable/grammar_lexer.py "$INPUT"
 CNFGRAMMAROUT="${FILENAME}_CNF.txt"
 touch "$CNFGRAMMAROUT"
-g++ main.cpp cnf.cpp -std=c++17 -o main
-./main "$LEXEROUT" "$CNFGRAMMAROUT"
+g++ executable/main.cpp executable/cnf.cpp -std=c++17 -o executable/main
+./executable/main "$LEXEROUT" "$CNFGRAMMAROUT"
